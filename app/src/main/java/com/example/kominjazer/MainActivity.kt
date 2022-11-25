e502af7dcea3e84fec0959a776ee9f166dd5fee3
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-11-25 11:01:37 +0100

    wybor miasta w drugim spinnerze dziala

--- app/src/main/java/com/example/kominjazer/MainActivity.kt
+++ app/src/main/java/com/example/kominjazer/MainActivity.kt
@@ -1,45 +1,45 @@
 package com.example.kominjazer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val spinnery = listOf(findViewById<Spinner>(R.id.spinner), findViewById<Spinner>(R.id.spinner2))
         val City = resources.getStringArray(R.array.City)
 
-        val spinner = this.findViewById<Spinner>(R.id.spinner)
-        if (spinner != null) {
-            val adapter = ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_item, City
-            )
-            spinner.adapter = adapter
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, City
+        )
+        spinnery[0].adapter = adapter
+        spinnery[1].adapter = adapter
 
-            spinner.onItemSelectedListener = object :
-                AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>,
-                    view: View, position: Int, id: Long
-                ) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        getString(R.string.selected_item) + " " +
-                                "" + City[position], Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>) {
-                    // write code to perform some action
-                }
+        spinnery[0].onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+                Toast.makeText(
+                 this@MainActivity,
+                 getString(R.string.selected_item) + " " +
+                         "" + City[position], Toast.LENGTH_SHORT
+             ).show()
             }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {
+            // write code to perform some action
         }
+    }
     }
 }
