fa797f49e62095b481e9adf81087b830b13ec6fb
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-11-25 11:20:37 +0100

    16 spinnerow z opcja wyboru miasta, brak opcji usuwania miast po wybraniu go po raz pierwszy

--- app/src/main/java/com/example/kominjazer/MainActivity.kt
+++ app/src/main/java/com/example/kominjazer/MainActivity.kt
@@ -14,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinnery = listOf(findViewById<Spinner>(R.id.spinner), findViewById<Spinner>(R.id.spinner2))
+        val spinnery = listOf(
+
+            findViewById<Spinner>(R.id.spinner),
+            findViewById<Spinner>(R.id.spinner2),
+            findViewById<Spinner>(R.id.spinner3),
+            findViewById<Spinner>(R.id.spinner4),
+            findViewById<Spinner>(R.id.spinner5),
+            findViewById<Spinner>(R.id.spinner6),
+            findViewById<Spinner>(R.id.spinner7),
+            findViewById<Spinner>(R.id.spinner8),
+            findViewById<Spinner>(R.id.spinner9),
+            findViewById<Spinner>(R.id.spinner10),
+            findViewById<Spinner>(R.id.spinner11),
+            findViewById<Spinner>(R.id.spinner12),
+            findViewById<Spinner>(R.id.spinner13),
+            findViewById<Spinner>(R.id.spinner14),
+            findViewById<Spinner>(R.id.spinner15),
+            findViewById<Spinner>(R.id.spinner16),
+        )
+
         val City = resources.getStringArray(R.array.City)
 
         val adapter = ArrayAdapter(
@@ -23,6 +42,20 @@ class MainActivity : AppCompatActivity() {
         )
         spinnery[0].adapter = adapter
         spinnery[1].adapter = adapter
+        spinnery[2].adapter = adapter
+        spinnery[3].adapter = adapter
+        spinnery[4].adapter = adapter
+        spinnery[5].adapter = adapter
+        spinnery[6].adapter = adapter
+        spinnery[7].adapter = adapter
+        spinnery[8].adapter = adapter
+        spinnery[9].adapter = adapter
+        spinnery[10].adapter = adapter
+        spinnery[11].adapter = adapter
+        spinnery[12].adapter = adapter
+        spinnery[13].adapter = adapter
+        spinnery[14].adapter = adapter
+        spinnery[15].adapter = adapter
 
         spinnery[0].onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
